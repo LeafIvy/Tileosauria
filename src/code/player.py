ebf04579591bab9cf1a307abe3b4b5a24888c1b5
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 14:50:00 +0530

    Add comments to Player Class

--- src/code/player.py
+++ src/code/player.py
@@ -15,17 +15,19 @@ class Player(pg.sprite.Sprite):
         self.direction = pg.Vector2()
 
     def input(self):
+        """Get input from keyboard and alter direction vector"""
         keys = pg.key.get_pressed()
         self.direction.x = int(keys[pg.K_d]) - int(keys[pg.K_a])
 
         self.direction.y = int(keys[pg.K_s]) - int(keys[pg.K_w])
 
     def move(self, dt):
+        """Alter position of player rect"""
         self.rect.x += self.speed * TILE_SIZE * self.direction.x * dt
 
         self.rect.y += self.speed * TILE_SIZE * self.direction.y * dt
 
     def update(self, dt):
+        """Call methods to update player data"""
         self.input()
         self.move(dt)
-        
