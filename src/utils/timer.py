0173b2e6c7681d3fe39f3527fa12ac2a300cdabf
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 15:41:26 +0530

    Add Timer class

---
+++ src/utils/timer.py
@@ -0,0 +1,27 @@
+from .settings import *
+
+
+class Timer:
+    """Class for handling timed events"""
+    def __init__(self, duration, func=None, autostart=False, repeat=False):
+        # base setup
+        self.active = False
+        self.duration = duration
+        self.start_time = 0
+        self.func = func
+        self.repeat = repeat
+        if autostart: self.activate()
+
+    def activate(self):
+        self.active = True
+        self.start_time = pg.time.get_ticks()
+
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+        if self.repeat: self.activate()
+
+    def update(self):
+        if pg.time.get_ticks() - self.start_time >= self.duration:
+            if self.func and self.start_time: self.func()
+            self.deactivate()
