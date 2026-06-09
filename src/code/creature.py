6a2ce121be333075ebaf74184730b1db807856c9
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 15:19:06 +0530

    Add health attribute to Creature

--- src/code/creature.py
+++ src/code/creature.py
@@ -11,6 +11,7 @@ class Creature(pg.sprite.Sprite):
         self.rect = self.image.get_frect(topleft=pos)
 
         # base attributes
+        self.health = 100
         self.comfort_lvl = 100 # range 0-100
         self.hunger_lvl = 100
         self.thirst_lvl = 100 # thirst satisfaction level, not thirstiness
