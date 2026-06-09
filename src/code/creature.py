5bdb0389f2b3c911c7a71a331311b513573fb754
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 14:31:39 +0530

    Create Creature class with base stats

---
+++ src/code/creature.py
@@ -0,0 +1,19 @@
+from src.utils import *
+
+
+class Creature(pg.sprite.Sprite):
+    """Base class for all creatures (flora and fauna)"""
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+
+        # base settings
+        self.image = surf
+        self.rect = self.image.get_frect(topleft=pos)
+
+        # base attributes
+        self.comfort_lvl = 100 # range 0-100
+        self.hunger_lvl = 100
+        self.thirst_lvl = 100 # thirst satisfaction level, not thirstiness
+        self.age = 100
+
+
