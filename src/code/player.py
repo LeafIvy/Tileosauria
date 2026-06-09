000d1717e4f873f04cb22bb8421d832990973ce4
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 16:02:19 +0530

    Add player movement based on input delay

--- src/code/player.py
+++ src/code/player.py
@@ -11,24 +11,28 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_frect(topleft=pos)
 
         # movement attributes
-        self.speed = 3 # tiles per second
+        move_delay_time = 250 # milliseconds
+        self.speed = 0.5 # tiles per {move_delay_time}
         self.direction = pg.Vector2()
-        self.move_delay = Timer(200)
+        self.move_delay = Timer(move_delay_time)
 
     def input(self):
         """Get input from keyboard and alter direction vector"""
         keys = pg.key.get_pressed()
         self.direction.x = int(keys[pg.K_d]) - int(keys[pg.K_a])
-
         self.direction.y = int(keys[pg.K_s]) - int(keys[pg.K_w])
 
     def move(self, dt):
         """Alter position of player rect"""
-        self.rect.x += self.speed * TILE_SIZE * self.direction.x * dt
+        if not self.move_delay:
+            self.rect.x += self.speed * TILE_SIZE * self.direction.x
 
-        self.rect.y += self.speed * TILE_SIZE * self.direction.y * dt
+            self.rect.y += self.speed * TILE_SIZE * self.direction.y
+
+            if self.direction: self.move_delay.activate()
 
     def update(self, dt):
         """Call methods to update player data"""
         self.input()
+        self.move_delay.update()
         self.move(dt)
