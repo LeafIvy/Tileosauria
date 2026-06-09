dfe433246ab0f5621677d6eefdd660cad1a3337d
Leaf Ivy <leafmasterivy@gmail.com>
2026-06-09 14:46:18 +0530

    Create input method in Player class

--- src/code/player.py
+++ src/code/player.py
@@ -9,4 +9,14 @@ class Player(pg.sprite.Sprite):
         # base settings
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
-        
+
+        # movement attributes
+        self.speed = 3 # tiles per second
+        self.direction = pg.Vector2()
+
+    def input(self):
+        keys = pg.key.get_pressed()
+        self.direction.x = int(keys[pg.K_d]) - int(keys[pg.K_a])
+
+        self.direction.y = int(keys[pg.K_s]) - int(keys[pg.K_w])
+
