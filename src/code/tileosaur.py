854fb9f2b4736522c7a88c2062137e2bab0685e4
Leaf Master Ivy <leafmasterivy@gmail.com>
2026-06-12 18:31:24 +0530

    Create an A* method

--- src/code/tileosaur.py
+++ src/code/tileosaur.py
@@ -27,18 +27,50 @@ class Tileosaur(Creature):
         centery = [y for y in range(int(self.rect.centery - self.range * TILE_SIZE),
                                     int(self.rect.centery + self.range * TILE_SIZE + 1), TILE_SIZE)]
         centers = [(x, y) for x in centerx for y in centery]
-        for pos in centers: self.nodes.append(TileNode(pos, math.inf))
+        for pos in centers: self.nodes.append(TileNode(pos))
 
-    def move(self, target):
-        """Uses A* to pathfind to target
-
-        target = position of center of tile
+    def astar(self, target):
         """
-        target_centerx, target_centery = target
+        Uses A* to pathfind to target and return next node to move to
+
+        target = Rect at goal
+        """
+        if target.collidelist(self.nodes) == -1: return self.rect   # don't move if target is outside of range
+
+        current_node = [node for node in self.nodes if node.center == self.rect.center][0]  # initial node at sprite's position
+        current_node.parent = current_node
+        current_node.distance_cost = math.sqrt((target.centerx - current_node.centerx) ** 2 + (target.centery - current_node.centery) ** 2)
+        # Initializing node distance_costs
+        for node in self.nodes:
+            node.distance_cost = math.sqrt((target.centerx - node.centerx) ** 2 + (target.centery - node.centery) ** 2)
+            if node.colliderect(current_node):
+                node.parent = current_node
+                node.cost = TILE_SIZE
+
+        leaf_nodes = [current_node]
+        while leaf_nodes:
+            leaf_nodes.remove(current_node)
+
+            if current_node.center == target.center:
+                while not current_node.colliderect(self.rect):
+                    current_node = current_node.parent
+                return current_node
+
+            for node in self.nodes:
+                if node.colliderect(current_node) and not node in leaf_nodes:
+                    if node.parent:
+                        if (node.cost + node.distance_cost) > (current_node.cost + TILE_SIZE + node.distance_cost):
+                            node.parent = current_node
+                            node.cost = current_node.cost + TILE_SIZE
+                    else:
+                        node.cost = current_node.cost + TILE_SIZE
+                    leaf_nodes.append(node)
+
+            current_node = min(leaf_nodes, key=lambda x: x.cost + x.distance_cost)
 
 class TileNode(pg.FRect):
-    def __init__(self, pos, cost):
+    def __init__(self, pos):
         super().__init__(pos, (TILE_SIZE + 5,) * 2)
         self.parent = None
-        self.cost = cost    # cost to target, i.e, h(n)
-        self.adjacent_tiles = {}    # dictionary of form {TileNode: heuristic} where heuristic is g(n)
+        self.cost = 0   # cost from initial point, i.e, g(n)
+        self.distance_cost = math.inf    # cost to target, i.e, h(n)
