a6d97f21d4fe207b06993bd34cf218876f07378c
Leaf Master Ivy <leafmasterivy@gmail.com>
2026-06-19 23:21:32 +0530

    Create Tile class for tiles; Create __iter__ in WorldGen

--- src/code/worldgen.py
+++ src/code/worldgen.py
@@ -8,6 +8,9 @@ class WorldGen:
         self.size = size
         self.world = {}
 
+    def __iter__(self):
+        return iter(self.world.items())
+
     def generate_perlin_noise(self, scale=100.0, octaves=6, persistance=0.3, lacunarity=2.0):
         for y in range(self.size):
             for x in range(self.size):
@@ -21,6 +24,11 @@ class WorldGen:
                     lacunarity=lacunarity
                 )
                 if noise_value < -0.5:
-                    tile = 'water'
-                else: tile = 'ground'
-                self.world[(x, y)] = tile
+                    color = 'blue'
+                else: color = 'green'
+                self.world[(x, y)] = Tile((x, y), (TILE_SIZE, TILE_SIZE), color)
+
+class Tile(pg.FRect):
+    def __init__(self, pos, size, color):
+        super().__init__(pos, size)
+        self.color = color
